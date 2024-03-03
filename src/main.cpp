28bc4ed056efe53c76d5f032c08818bca5b24790
Alan <alanff15@gmail.com>
2024-03-03 19:51:24 -0300

    Threads encadeadas

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,20 @@
 
 #include "SwapBuffer.h"
 
-typedef struct DATA_T {
-  int val1 = 0;
-  float val2 = 0.0f;
-  int v[5] = {100, 200, 300, 400, 500};
-} data_t;
+int globalCount = 0;
+bool run = true;
+
+typedef struct DATA_A_T {
+  int val = 0;
+} data_A_t;
+
+typedef struct DATA_B_T {
+  float val = 0.0f;
+} data_B_t;
+
+typedef struct DATA_C_T {
+  double val = 0;
+} data_C_t;
 
 float getDeltaTime() {
   static std::chrono::time_point<std::chrono::steady_clock> t1, t0 = std::chrono::high_resolution_clock::now();
@@ -19,47 +28,74 @@ float getDeltaTime() {
   return delta;
 }
 
-void threadWrite(SwapBuffer<data_t>* buf) {
-  while (true) {
-    // pegar buffer
-    data_t* data = buf->getWriteBuffer();
-    // escrever dados
-    Sleep(500);
-    data->val1++;
-    data->val2 += 0.1f;
-    for (int i = 0; i < 5; i++) data->v[i]--;
-    // sinalizar final da escrita
-    buf->finishWrite();
+///////////////////////////////////////////////////////////////////////////////
+
+void threadA(SwapBuffer<data_A_t>* out) {
+  while (run) {
+    data_A_t* dataOut = out->getWriteBuffer();
+    Sleep(100);
+    dataOut->val = globalCount;
+    out->finishWrite();
   }
 }
 
-void threadRead(SwapBuffer<data_t>* buf) {
-  while (true) {
-    // pegar buffer
-    data_t* data = buf->getReadBuffer();
-    // ler dados
-    Sleep(1200);
-    std::cout << "val1=" << data->val1 << "\t" << std::ends;
-    std::cout << "val2=" << ++(data->val2) << "\t" << std::ends;
-    std::cout << "v=[" << std::ends;
-    for (int i = 0; i < 5; i++) std::cout << data->v[i] << "\t" << std::ends;
-    std::cout << "]" << std::ends;
-    std::cout << " tempo=" << getDeltaTime() << std::endl;
-    // sinalizar final da leitura do buffer
-    buf->finishRead();
+void threadB(SwapBuffer<data_A_t>* in, SwapBuffer<data_B_t>* out) {
+  while (run) {
+    data_A_t* dataIn = in->getReadBuffer();
+    data_B_t* dataOut = out->getWriteBuffer();
+    Sleep(100);
+    dataOut->val = dataIn->val + 0.5f;
+    in->finishRead();
+    out->finishWrite();
   }
 }
+
+void threadC(SwapBuffer<data_B_t>* in, SwapBuffer<data_C_t>* out) {
+  while (run) {
+    data_B_t* dataIn = in->getReadBuffer();
+    data_C_t* dataOut = out->getWriteBuffer();
+    Sleep(100);
+    dataOut->val = dataIn->val + 0.025;
+    in->finishRead();
+    out->finishWrite();
+  }
+}
+
+void threadD(SwapBuffer<data_C_t>* in) {
+  while (run) {
+    data_C_t* dataIn = in->getReadBuffer();
+    Sleep(100);
+    std::cout << "val = " << dataIn->val << "\ttempo = " << getDeltaTime() << std::endl;
+    if (GetKeyState(VK_ESCAPE) & 0x8000) run = false;
+    globalCount++;
+    in->finishRead();
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 int main() {
-  SwapBuffer<data_t> buf(true);
+  SwapBuffer<data_A_t> bufAB(true);
+  SwapBuffer<data_B_t> bufBC(true);
+  SwapBuffer<data_C_t> bufCD(true);
 
   getDeltaTime();  // iniciar timer
 
-  std::thread th_r([&buf]() { threadRead(&buf); });
-  std::thread th_w([&buf]() { threadWrite(&buf); });
+  std::thread th_A([&bufAB]() { threadA(&bufAB); });
+  std::thread th_B([&bufAB, &bufBC]() { threadB(&bufAB, &bufBC); });
+  std::thread th_C([&bufBC, &bufCD]() { threadC(&bufBC, &bufCD); });
+  std::thread th_D([&bufCD]() { threadD(&bufCD); });
 
-  if (th_r.joinable()) th_r.join();
-  if (th_w.joinable()) th_w.join();
+  if (th_D.joinable()) th_D.join();
+  bufAB.killThreads();
+  bufBC.killThreads();
+  bufAB.killThreads();
+  if (th_C.joinable()) th_C.join();
+  if (th_B.joinable()) th_B.join();
+  if (th_A.joinable()) th_A.join();
+
+  std::cout << "Final das threads" << std::endl;
+  std::cout << "globalCount = " << globalCount << std::endl;
 
   return EXIT_SUCCESS;
 }
